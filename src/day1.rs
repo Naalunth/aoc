54b63645e87f414dae2f3cf4af156d6d1bebef0a
Naalunth <kevin-schier@web.de>
2018-12-02 17:55:40 +0100

    Days 1, 2 done

---
+++ src/day1.rs
@@ -0,0 +1,74 @@
+type GeneratorOut = Vec<i64>;
+
+#[aoc_generator(day1)]
+pub fn gen(input: &str) -> GeneratorOut {
+	input.lines()
+		.map(|l| l.parse::<i64>().unwrap())
+		.collect::<Vec<_>>()
+}
+
+#[aoc(day1, part1)]
+pub fn p1(input: &GeneratorOut) -> i64 {
+	input.iter().sum::<i64>()
+}
+
+#[aoc(day1, part2)]
+pub fn p2(input: &GeneratorOut) -> i64 {
+	use std::collections::{HashSet, HashMap};
+
+	let mut freqs = std::iter::once(0)
+		.chain(
+			input.iter()
+			.scan(0, |state, &i| {
+				*state += i;
+				Some(*state)
+			})
+		)
+		.collect::<Vec<_>>();
+
+	if let Some(repetition) = freqs.iter()
+		.scan(HashSet::new(), |state, &current_frequency| {
+			Some(if state.insert(current_frequency) {
+				None
+			} else {
+				Some(current_frequency)
+			})
+		})
+		.filter_map(|x| x)
+		.nth(0)
+	{
+		return repetition;
+	}
+
+	let freq_shift = freqs.pop().unwrap();
+	if freq_shift == 0 {
+		return 0;
+	}
+
+	let mut groups = HashMap::new();
+	for (i, &freq) in freqs.iter().enumerate() {
+		groups.entry(freq % freq_shift)
+			.or_insert_with(|| Vec::<(usize, i64)>::new())
+			.push((i, freq));
+	}
+
+	let mut min_diff: Option<i64> = None;
+	let mut min_freq = 0i64;
+	let mut min_index = 0usize;
+
+	for group in groups.values_mut() {
+		group.sort_unstable_by_key(|e| e.1);
+		for i in 1..group.len() {
+			let diff = group[i].1 - group[i - 1].1;
+			let index = if freq_shift > 0 {group[i-1].0} else {group[i].0};
+			let freq = if freq_shift > 0 {group[i].1} else {group[i-1].1};
+			if min_diff.is_none() || diff < min_diff.unwrap() || (diff == min_diff.unwrap() && index < min_index) {
+				min_diff = Some(diff);
+				min_freq = freq;
+				min_index = index;
+			}
+		}
+	}
+
+	min_freq
+}
---
+++ src/day2.rs
@@ -0,0 +1,65 @@
+type GeneratorOut = Vec<String>;
+
+use std::collections::{HashMap};
+
+#[aoc_generator(day2)]
+pub fn gen(input: &str) -> GeneratorOut {
+	input.lines().map(|l| l.to_owned()).collect::<Vec<_>>()
+}
+
+#[aoc(day2, part1)]
+pub fn p1(input: &GeneratorOut) -> u32 {
+	let (twos, threes) = input.iter()
+		.map(|id| {
+			let mut two = false;
+			let mut three = false;
+			for &n in id.chars()
+				.fold(HashMap::new(), |mut map, c| {
+					*map.entry(c).or_insert_with(|| 0u32) += 1;
+					map
+				})
+				.values()
+			{
+				two = two || n == 2;
+				three = three || n == 3;
+				if two && three {break;}
+			}
+			(if two {1} else {0}, if three {1} else {0})
+		})
+		.fold((0u32, 0u32), |(c2, c3), (n2, n3)| {
+			(c2 + n2, c3 + n3)
+		});
+	twos * threes
+}
+
+#[aoc(day2, part2)]
+pub fn p2(input: &GeneratorOut) -> String {
+	let mut sorted = input.to_vec();
+	sorted.sort();
+	let (a, b) = (0..sorted.len())
+		.flat_map(|i|
+			sorted[i..].iter()
+				.zip(&sorted)
+		)
+		.find(|(a, b)| differ_by_one(a, b))
+		.unwrap();
+	common_chars(a, b)
+}
+
+fn differ_by_one(a: &str, b: &str) -> bool {
+	let mut iter = a.chars()
+		.zip(b.chars())
+		.filter(|(ac, bc)| ac != bc);
+	iter.next().is_some() && iter.next().is_none()
+}
+
+fn common_chars(a: &str, b: &str) -> String {
+	a.chars()
+		.zip(b.chars())
+		.filter_map(|(ac, bc)| if ac == bc {
+			Some(ac)
+		} else {
+			None
+		})
+		.collect::<String>()
+}
