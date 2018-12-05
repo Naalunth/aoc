978c585ba9f2ac04a5691f514222f3e2338c11b6
Naalunth <kevin-schier@web.de>
2018-12-05 07:12:31 +0100

    using a stack or day 5

--- src/day5.rs
+++ src/day5.rs
@@ -6,34 +6,22 @@ fn opposite_units(a: u8, b: u8) -> bool {
 	}
 }
 
-fn collapsed_length(polymer: &[u8]) -> usize {
-	let mut copy = polymer.to_vec();
-	let mut i = 1;
-	let mut removed_one = false;
-	loop {
-		while i < copy.len() {
-			if opposite_units(copy[i-1], copy[i]) {
-				copy.remove(i);
-				copy.remove(i-1);
-				removed_one = true;
-			} else {
-				i += 1;
-			}
-		}
-		if !removed_one {
-			break;
+fn collapse_length(polymer: &[u8]) -> usize {
+	let mut stack = Vec::new();
+	for unit in polymer {
+		if stack.last().map_or(false, |last| opposite_units(*last, *unit)) {
+			stack.pop();
 		} else {
-			i = 1;
-			removed_one = false;
+			stack.push(*unit);
 		}
 	}
-	copy.len()
+	stack.len()
 }
 
 #[aoc(day5, part1)]
 pub fn part_1(input: &[u8]) -> usize {
 	let copy = input.iter().filter(|c| u8::is_ascii_alphabetic(c)).cloned().collect::<Vec<_>>();
-	collapsed_length(&copy)
+	collapse_length(&copy)
 }
 
 #[aoc(day5, part2)]
@@ -43,7 +31,7 @@ pub fn part_2(input: &[u8]) -> usize {
 			.filter(|c| c.is_ascii_alphabetic() && !c.eq_ignore_ascii_case(&deletion))
 			.cloned()
 			.collect::<Vec<_>>();
-		collapsed_length(&copy)
+		collapse_length(&copy)
 	}).min().unwrap()
 }
 
